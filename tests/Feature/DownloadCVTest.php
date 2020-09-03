4d8c0e446d2fa62d4030fbedeb9fbb7feab7dc28
Kurt Friars <kfriars@gmail.com>
2020-09-03 13:11:22 -0400

    master Handle resume pdf on ci

--- tests/Feature/DownloadCVTest.php
+++ tests/Feature/DownloadCVTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use Barryvdh\DomPDF\PDF;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Filesystem\FilesystemManager;
 use Tests\TestCase;
 
 class DownloadCVTest extends TestCase
@@ -9,6 +12,17 @@ class DownloadCVTest extends TestCase
     /** @test */
     public function it_downloads_the_cv_using_a_valid_recaptcha_token()
     {
+        /** @var Filesystem */
+        $filesystem = $this->app->make(FilesystemManager::class)->createLocalDriver([ 'root' => storage_path() ]);
+
+        // Handle CI where the file won't exist
+        if (! $filesystem->exists('private/Kurt Friars - Resume.pdf')) {
+            $this->app->make(PDF::class)->loadHTML('<h1>Test Resume</h1>')
+                ->setPaper('a4')
+                ->setWarnings(false)
+                ->save(storage_path("private/'Kurt Friars - Resume.pdf'"));
+        }
+
         $response = $this->get('cv/test-recaptcha-token');
         $response->assertStatus(200);
     }
